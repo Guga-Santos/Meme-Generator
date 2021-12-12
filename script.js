884c0c7494eb2d56b155c26722bf4bb83b5d14b1
GugaSantos <guga.santos.guga@gmail.com>
2021-12-12 14:51:52 -0300

    Requisito 06 ok

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ const carregaImagem = (event) => {
   const imagem = document.getElementById('image-input');
   imagem.src = URL.createObjectURL(event.target.files[0]);
 };
+
+//  Retirei a ideia de fazer o upload da imagem daqui:
+//  https://www.webtrickshome.com/forum/how-to-display-uploaded-image-in-html-using-javascript
+//  Utilizei este outro link pra estudo.
+//  https://developer.mozilla.org/en-US/docs/Web/API/File/Using_files_from_web_applications#example.3a_using_object_urls_to_display_images
+
+const fireButton = document.querySelector('#fire');
+const waterButton = document.querySelector('#water');
+const earthButton = document.querySelector('#earth');
+const imageContainer = document.querySelector('#meme-image-container');
+
+fireButton.addEventListener('click', () => {
+  imageContainer.style.border = '3px dashed red';
+});
+
+waterButton.addEventListener('click', () => {
+  imageContainer.style.border = '5px double blue';
+});
+
+earthButton.addEventListener('click', () => {
+  imageContainer.style.border = '6px groove green';
+});
